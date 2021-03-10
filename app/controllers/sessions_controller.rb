30877aafc86d95a6ebce5377a5e804eb8ec5895b
Hareesh <mutyala.hareesh341@gmail.com>
2021-03-10 08:31:12 +0530

    Login App

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+ 	skip_before_action :authorize
+  def new
+  end
+
+  def create
+  	user = user.find_by(name: params[:name])
+  	if user and user.authenticate(params[:password])
+  		sessions[:user_id] = user.id
+  		redirect_to admin_url
+  	else
+  		redirect_to login_url, alert:"Invalid Username or Password"
+  	end
+
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to login_url, alert:"Successfully Logged Out"
+  end
+end
